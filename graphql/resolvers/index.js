4a483f362570f20350092eca03913e5c36f7e5a6
Alexander Grimes <66704965+Alex-Grimes@users.noreply.github.com>
2021-05-03 21:17:16 -0400

    graphql functionality
    added in a couple of resolver folders under graph ql as well as setting up user authentication useing bcrypt and json web tokens

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,11 @@
+const postResolvers = require('./posts');
+const userResolvers = require('.users');
+
+module.exports = {
+    Query: {
+        ...postsResolvers.Query
+    },
+    Mutation: {
+        ...userResolvers.Mutation
+    }
+}
